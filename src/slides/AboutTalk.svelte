3ee9e1954f1ca41fda0d71841e485c8a1b4984e0
Martin Watts <martin.watts.dev@gmail.com>
2023-01-09 12:08:13 +0000

    Update AboutTalk slide

--- src/slides/AboutTalk.svelte
+++ src/slides/AboutTalk.svelte
@@ -7,9 +7,9 @@
     About the talk:
   </h2>
   <ul>
-    <li class="fragment fade-up">It's <em>a</em> history of CSS, not the <em>entire</em> history.</li>
-    <li class="fragment fade-up">It's designed to give you some context about the language.</li>
-    <li class="fragment fade-up">It's not too code heavy.</li>
+    <li class="fragment fade-up mb-5">It's <em>a</em> history of CSS, not the <em>entire</em> history.</li>
+    <li class="fragment fade-up mb-5">It's designed to give you some context about the language - how it came to be, and how it has evolved.</li>
+    <li class="fragment fade-up mb-5">It's not <em>too</em> code heavy, but I'm assuming you've seen some sort of code before.</li>
   </ul>
 </Slide>
 
