d1f0997ff2a325e911177cdf40683a8a863caab3
Martin Watts <martin.watts@mayden.co.uk>
2023-01-15 20:54:53 +0000

    Add an animated timeline component

---
+++ src/lib/Timeline.svelte
@@ -0,0 +1,22 @@
+<style>
+  ol {
+    display: flex !important;
+    list-style: none !important;
+  }
+
+  ol::before {
+    background-color: var(--timeline-color, white);
+    content: ' ';
+    display: block;
+    height: var(--timeline-height, 2px);
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 10px;
+  }
+</style>
+
+<ol>
+  <slot />
+</ol>
+
---
+++ src/lib/TimelineItem.svelte
@@ -0,0 +1,141 @@
+<style>
+  li {
+    position: relative;
+    text-align: center;
+    width: var(--width, 22rem);
+    opacity: var(--opacity, 1);
+    transition: all;
+    transition-duration: 0.5s;
+  }
+
+  div {
+    margin-right: var(--margin-right, 3rem);
+    opacity: var(--opacity, 100);
+    transition: all;
+    transition-duration: 1.5s;
+    transition-delay: 1.75s;
+  }
+
+  h3 {
+    color: var(--title-color, var(--pacificBlue)) !important;
+  }
+
+  svg {
+    height: var(--icon-height, 2.5rem);
+    margin-top: var(--icon-margin-top, -0.5rem);
+    z-index: 10;
+    width: 100%;
+    margin-right: -var(--margin-right, 3rem);
+  }
+
+  circle {
+    fill: var(--icon-color, var(--oliveDrab3));
+    stroke: var(--icon-stroke-color, white);
+    stroke-width: var(--icon-stroke-width, 5px);
+  }
+
+  line {
+    stroke: var(--line-stroke-color, var(--oliveDrab3));
+    stroke-width: var(--line-stroke-width, 5px);
+  }
+</style>
+
+<script>
+  let svgElement;
+
+  export let title;
+  export let date;
+  export let iconX = 'calc(50% - 1.5rem)';
+  export let iconY = 20;
+  export let iconSize = 15;
+  export let animateIn = false;
+  export let delayIn = true;
+  export let animateOut = false;
+  export let beforeLine = false;
+  export let afterLine = false;
+  export const restartAnimation = () => {
+    if (!svgElement) {
+      return;
+    }
+
+    const animations = svgElement.querySelectorAll('animate');
+
+    animations.forEach(animation => {
+      const delay = animation.getAttribute('begin');
+
+      if (delay) {
+        animation.parentElement.style.display='none';
+        const removeDisplayNone = () => animation.parentElement.style.display = '';
+
+        animation.addEventListener('beginEvent', removeDisplayNone, { once: true })
+        animation.beginElementAt(delay.replace('s', ''));
+      } else {
+        animation.beginElement();
+      }
+    });
+  }
+</script>
+
+
+
+<li>
+  {#if $$slots.icon}
+    <slot name="icon" />
+  {:else}
+    <svg bind:this={svgElement}>
+      {#if beforeLine}
+        <line x1="0%" y1={iconY} x2={animateIn ? null : '48%'} y2={iconY}>
+          {#if animateIn}
+            <animate
+              attributeName="x2"
+              from="0%"
+              to="48%"
+              begin={delayIn ? '0.75s' : null}
+              dur="0.75s"
+              fill="freeze"
+            />
+          {/if}
+        </line>
+      {/if}
+      {#if afterLine}
+        <line x1="48%" y1={iconY} x2={animateOut ? null : '100%'} y2={iconY}>
+          {#if animateOut}
+            <animate
+              attributeName="x2"
+              from="48%"
+              to="100%"
+              dur="0.75s"
+              fill="freeze"
+            />
+          {/if}
+        </line>
+      {/if}
+      <circle cx={iconX} cy={iconY} r={iconSize}>
+        {#if animateIn}
+          <animate
+            attributeName="r"
+            from="0"
+            to={iconSize}
+            dur="0.25s"
+            begin={delayIn ? '1.5s' : '0.75s'}
+            fill="freeze"
+          />
+        {/if}
+      </circle>
+    </svg>
+  {/if}
+  <div style={!delayIn && 'transition-delay: 1.25s'}>
+    {#if title}
+      <h3>
+        {title}
+      </h3>
+    {/if}
+    {#if date}
+      <p>
+        {date}
+      </p>
+    {/if}
+    <slot />
+  </div>
+</li>
+
