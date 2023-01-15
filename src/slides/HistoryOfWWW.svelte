e95a05dffdec74c5d6858b6ee7c23ddd6f29efa3
Martin Watts <martin.watts@mayden.co.uk>
2023-01-15 20:56:57 +0000

    Add a history of the World Wide Web slide

---
+++ src/slides/HistoryOfWWW.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Slide from '../lib/Slide.svelte'; 
+</script>
+
+<Slide>
+  <p>6th August 1991, Tim Berners-Lee announces <a href="https://web.archive.org/web/20130529231641/https://groups.google.com/group/alt.hypertext/msg/395f282a67a1916c" rel="external">The WorldWideWeb</a> to the world. It's a development of three key technologies:</p> 
+  <ol class="pl-5">
+    <li class="mb-5"><span class="text-color-eggshell">Universal Document Identifiers</span> (later URLs) - how documents should be found.</li>
+    <li class="mb-5"><span class="text-color-flame">HyperText Markup Language</span> - how documents should be structured.</li>
+    <li ><span class="text-color-olive">HyperText Transfer Protocol</span> - how documents should be requested/returned from web servers.</li>
+  </ol>
+
+  <p class="fragment fade-up">But what about 'how documents should be styled'? 🤷</p>
+</Slide>
+
