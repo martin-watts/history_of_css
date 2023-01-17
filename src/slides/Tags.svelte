2ff3509f480d4a0a68030aebc73133cf7336dd2b
Martin Watts <martin.watts.dev@gmail.com>
2023-01-17 07:58:43 +0000

    Fix code highlighting in the tags slide

--- src/slides/Tags.svelte
+++ src/slides/Tags.svelte
@@ -6,15 +6,59 @@
   <h2>
     Tags to the rescue?
   </h2>
-  <p class="text-[36px]">The original idea behind HTML was that it was purely structural, but this was at odds with authors wanting more control.</p> 
-  <p class="text-[36px]">Over the next few years a bunch of <a href="https://www.w3.org/MarkUp/HTMLPlus/htmlplus_16.html" rel="external">presentational tags</a> were added to HTML such as:</p>
+  <p class="text-[32px]">
+    HTML was designed to be purely structural, but this was at odds with authors wanting more control.
+    Over the next few years a bunch of <a href="https://www.w3.org/MarkUp/HTMLPlus/htmlplus_16.html" rel="external">presentational tags</a> 
+    were created such as:
+  </p>
   <ul class="list-none">
-    <li><code>&lt;b&gt;</code><b>The boldface tag</b></li>
-    <li><code>&lt;i&gt;</code><i>The italic tag</i></li>
-    <li><code>&lt;u&gt;</code><u>The underline tag</u></li>
-    <li><code>&lt;s&gt;</code><s>The strike through tag</s></li>
-    <li><code>&lt;sup&gt;</code><sup>The superscript tag</sup></li>
-    <li><code>&lt;sub&gt;</code><sub>The subscript tag</sub></li>
+    <li>
+      <pre class="inline-block w-28 mr-5 mt-5">
+        <code data-trim class="hljs data-trim">{@html `<b>`}
+        </code>
+      </pre>
+      <b>The boldface tag</b>
+    </li>
+    <li>
+      <pre class="inline-block w-28 mr-5 mt-5">
+        <code data-trim class="hljs">
+          {@html `<i>`}
+        </code>
+      </pre>
+      <i>The italic tag</i>
+    </li>
+    <li>
+      <pre class="inline-block w-28 mr-5 mt-5">
+        <code data-trim class="hljs">
+          {@html `<u>`}
+        </code>
+      </pre>
+      <u>The underline tag</u>
+    </li>
+    <li>
+      <pre class="inline-block w-28 mr-5 mt-5">
+        <code data-trim class="hljs">
+          {@html `<s>`}
+        </code>
+      </pre>
+      <s>The strike through tag</s>
+    </li>
+    <li>
+      <pre class="inline-block w-40 mr-5 mt-5">
+        <code data-trim class="hljs">
+          {@html `<sup>`}
+        </code>
+      </pre>
+      <sup>The superscript tag</sup>
+    </li>
+    <li>
+      <pre class="inline-block w-40 mr-5 mt-5">
+        <code data-trim class="hljs data-trim">
+          {@html `<sub>`}
+        </code>
+      </pre>
+      <sub>The subscript tag</sub>
+    </li>
   </ul>
 </Slide>
 
