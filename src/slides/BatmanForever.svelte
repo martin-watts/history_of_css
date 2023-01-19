9cd647f5a28d4092bf57aa596fc6f0a73ebf2f9e
martin-watts <martin.watts.dev@gmail.com>
2023-01-19 09:53:52 +0000

    Reformat the BatmanForever slide

--- src/slides/BatmanForever.svelte
+++ src/slides/BatmanForever.svelte
@@ -5,20 +5,20 @@
 
 <Slide data-transition="slide-in">
   <h2>
-    ...and then the web got cool...
+    And then the web got cool...
   </h2>
-  <img class="object-contain w-[48rem] m-auto" src="/assets/images/batman_forever_1.webp" alt="batmanforver.com in 1995 - top of the site with batman logotypes and images"/>
+  <img class="object-contain w-[47rem] m-auto" src="/assets/images/batman_forever_1.webp" alt="batmanforver.com in 1995 - top of the site with batman logotypes and images"/>
 </Slide>
 <Slide>
   <h2>
     ...really...
   </h2>
-  <img class="object-contain w-[48rem] m-auto" src="/assets/images/batman_forever_2.webp" alt="batmanforver.com in 1995 - cinema section with downloadable movie clips"/>
+  <img class="object-contain w-[47rem] m-auto" src="/assets/images/batman_forever_2.webp" alt="batmanforver.com in 1995 - cinema section with downloadable movie clips"/>
 </Slide>
 <Slide>
   <h2>
     ...really cool!
   </h2>
-  <img class="object-contain w-[48rem] m-auto" src="/assets/images/batman_forever_3.webp" alt="batmanforver.com in 1995 - batcave section"/>
+  <img class="object-contain w-[47rem] m-auto" src="/assets/images/batman_forever_3.webp" alt="batmanforver.com in 1995 - batcave section"/>
 </Slide>
 
